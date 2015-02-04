b86d20ac014acf39eff54401095ec825cf3f6e7f
littleniu <fgyong@yeah.net>
2015-02-04 21:08:26 +0800

    Initial Commit

---
+++ TableViewPlain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewPlain
+//
+//  Created by 一叶知秋 on 15/2/4.
+//  Copyright (c) 2015年 小叶. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewPlain/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewPlain
+//
+//  Created by 一叶知秋 on 15/2/4.
+//  Copyright (c) 2015年 小叶. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
